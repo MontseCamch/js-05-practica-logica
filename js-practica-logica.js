c12b8a1036cb2c7ef12909b26272de9a46e07c69
MontseCamch <montserratcamch@gmail.com>
2024-08-04 23:05:56 -0600

    finalizacion de ejercicios de practica de logica

--- js-practica-logica.js
+++ js-practica-logica.js
@@ -1,32 +1,59 @@
 //User profile
+let user = prompt("Introduce tu usuario");
+let age = prompt("¿Cuál es tu edad?");
+let film = prompt("¿Cuál es tu película favorita?");
+
+function displayInformation(){
+    console.log(`Usuario: ${user}`);
+    console.log(`Edad: ${age}`);
+    console.log(`La película ${film} es una de mis favoritas.`);
+}
+
+displayInformation();
+//-------------------
 
 //Highest number
+let indx = 1;
+let highest=0;
+
+while (indx <= 10) {
+  let input = parseInt (prompt("Ingresar número:"));
+  if(input > highest){
+    highest = input;
+  } else {
+    highest = highest;
+  }
+  indx++;
+}
+
+console.log(`El número mayor es: ${highest}`);
+
 
 
 //Alarm
-/*let seconds= prompt("Introduce la cantidad de segundos para programar la alarma:");
+let seconds= prompt("Introduce la cantidad de segundos para programar la alarma:");
 function getUser(){
     console.log('¡Time to go to bed!');
 }
 
 setTimeout(getUser, seconds * 1000);
-console.log(`Time for bed after ${seconds} seconds`);*/
+console.log(`Time for bed after ${seconds} seconds`);
 //-------------------
 
 //Palindrome
-/*let text = prompt("Introduce una palabra o frase");
+let text = prompt("Introduce una palabra o frase");
 let sentence = text.toLowerCase();
 function palindrome() {
     const invertedText = sentence.split("").reverse().join("");
     if (sentence === invertedText) {
       console.log("Es palindromo");
     } else {
-      console.log("No es palindromo");"
+      console.log("No es palindromo");
     };
   
   };
   
-palindrome();*/
+palindrome();
 //-------------------
 
 //Factorial
